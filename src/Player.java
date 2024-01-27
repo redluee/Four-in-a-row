77e1aebbd9a450360028cfa762d20a34d61092ee
Redlue <steven24koning@gmail.com>
2024-01-28 00:05:16 +0100

    Creation of player class

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,28 @@
 
 public class Player {
-
+	private String name;
+	private int wins;
+	private String sign;
+	
+	public Player(String name, String sign) {
+		this.name = name;
+		wins = 0;
+		this.sign = sign;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getWins() {
+		return wins;
+	}
+	
+	public String getSign() {
+		return sign;
+	}
+	
+	public void addWin() {
+		wins ++;
+	}
 }
